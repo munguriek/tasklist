7ceb6290e5330af5764256bc8b1bad836e39ee83
munguriek <benjaminmunguriek@gmail.com>
2020-11-01 17:41:46 +0300

    Filter tasks

--- js/app.js
+++ js/app.js
@@ -15,6 +15,8 @@ function loadEventListeners(){
     taskList.addEventListener('click', removeTask);
     //Clear task Event
     clearBtn.addEventListener('click', clearTasks);
+    //Filter tasks Event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e){
@@ -49,8 +51,20 @@ function removeTask(e){
     }
 }
 
-function clearTasks(e){
+function clearTasks(){
     while(taskList.firstChild){
         taskList.removeChild(taskList.firstChild);
     }
+}
+
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
